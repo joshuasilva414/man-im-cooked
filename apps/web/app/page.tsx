6ebb9fa688a8423a3a6b441a3c0d4a6ee7eafaec
Soren <sor3nnw@gmail.com>
2025-01-25 14:50:10 -0600

    working i think added shadcn again

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,43 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-24 w-full">
-      <h1 className="text-4xl font-bold">
+    <div className="flex items-center  flex-col h-screen w-full">
+      <h1 className="text-4xl font-bold p-4">
         ManImCooked
       </h1>
+
+      <Accordion type="single" collapsible className="w-1/2">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
     </div>
   );
 }
