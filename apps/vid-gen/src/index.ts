294a23793f6d00cbac24753b8379773e6a4e4c87
Soren <sor3nnw@gmail.com>
2025-01-25 20:12:38 -0600

    updated main prompt

--- apps/vid-gen/src/index.ts
+++ apps/vid-gen/src/index.ts
@@ -22,8 +22,23 @@ app.get("/generate", async (c) => {
       messages: [
         {
           role: "system",
-          content:
-            "you are a helpful math robot that outputs manim python code for the given prompt. make sure you only respond with python code and that the code will run and have a result without modification. make sure the code is not commente and does not have ''' before or after the code. Also make sure that everything is essentially in one scene by clearing the screen before you start a new screen. feel free to use the voice over function to create voice over explanations of each scene",
+          content: `
+            you are a helpful math robot that outputs manim python code for the given prompt. 
+            make sure you only respond with python code and that the code will run and 
+            have a result without modification. make sure the code is not commented and does not have 
+            three apostrophies before or after the code. Also make sure that everything is 
+            essentially in one scene by clearing the screen before you start a new screen. feel 
+            free to use the voice over function to create voice over explanations of each scene. here is some example code of a voiceover being used to voice over a circle being drawn:
+            from manim_voiceover import VoiceoverScene
+            from manim_voiceover.services.gtts import GTTSService
+
+            class MyAwesomeScene(VoiceoverScene):
+            def construct(self):
+              self.set_speech_service(GTTSService())
+
+              with self.voiceover(text="This circle is drawn as I speak.") as tracker:
+                  self.play(Create(circle))
+            `,
         },
         { role: "user", content: userQuery.toString() },
       ],
